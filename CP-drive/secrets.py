eaaa693f9b9de3275c261142533b5aee315aeffa
Percy <pyzahl@ymail.com>
2023-01-12 20:31:21 -0500

    Add files via upload

---
+++ CP-drive/secrets.py
@@ -0,0 +1,13 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'YOURNETWORKSSID',
+    'password' : 'YOURWIFIPASSWORD',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'broker' : 'io.adafruit.com', # or your own local broker's IP
+    'port' : 1883,
+    'aio_username' : 'username',
+    'aio_key' : 'aio_userkeykeykeyxxxxxxx',
+    }
+    
